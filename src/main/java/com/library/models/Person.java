ab23aba7536b940c3ca448b0dd0448486789bc59
Kyrylo Gorbunov <kyryl1504@gmail.com>
2022-11-05 12:10:35 +0100

    Migrate project from jdbcTemplate to Hibernate and Spring Data JPA.

--- src/main/java/com/library/models/Person.java
+++ src/main/java/com/library/models/Person.java
@@ -1,18 +1,32 @@
 package com.library.models;
 
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.List;
 
+@Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
 
     @NotEmpty(message = "Name must not be empty")
     @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
+    @Column(name = "full_name")
     private String fullName;
 
     @Min(value = 1900, message = "Year of birth must be greater than 1900")
+    @Column(name = "year_of_birth")
     private int yearOfBirth;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
 
     public Person() {
 
@@ -45,6 +59,14 @@ public class Person {
 
     public void setYearOfBirth(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 }
 
--- src/main/java/com/library/util/PersonValidator.java
+++ src/main/java/com/library/util/PersonValidator.java
@@ -1,7 +1,7 @@
 package com.library.util;
 
-import com.library.dao.PersonDAO;
 import com.library.models.Person;
+import com.library.services.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -9,11 +9,12 @@ import org.springframework.validation.Validator;
 
 @Component
 public class PersonValidator implements Validator {
-    private final PersonDAO personDAO;
+
+    private final PeopleService peopleService;
 
     @Autowired
-    public PersonValidator(PersonDAO personDAO) {
-        this.personDAO = personDAO;
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
     }
 
     @Override
@@ -25,7 +26,7 @@ public class PersonValidator implements Validator {
     public void validate(Object o, Errors errors) {
         Person person = (Person) o;
 
-        if (personDAO.getPersonByFullName(person.getFullName()).isPresent())
+        if (peopleService.getPersonByFullName(person.getFullName()).isPresent())
             errors.rejectValue("fullName", "", "A person with this name already exists");
     }
 }
